63b98ecb7f211e0b20f5b2dbbd65679925301dbe
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-29 14:55:09 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,5 +7,13 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  
+  def new 
+  end
+  
+  def create 
+    Student.create(params)
+    redirect_to students_path
+  end
 
 end
